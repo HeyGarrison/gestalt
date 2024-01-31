586df15f3bd239304a0672e77f97d6389dc74734
HeyGarrison <garrison@hey.com>
2024-01-31 15:57:36 -0600

    more!

--- scripts/stackblitz.sh
+++ scripts/stackblitz.sh
@@ -1,13 +1,10 @@
 #!/usr/bin/env sh
 
-# Simplified start script for the stack blitz environment, which can't handle multiple watchers
-set -x
-set -e
 
-# (cd packages/gestalt-design-tokens && pnpm build)
-# (cd packages/gestalt && pnpm build)
-# (cd packages/gestalt-charts && pnpm build)
-# (cd packages/gestalt-datepicker && pnpm build)
-# (cd docs && pnpm build-metadata)
+(cd packages/gestalt-design-tokens && pnpm build)
+(cd packages/gestalt && pnpm build)
+(cd packages/gestalt-charts && pnpm build)
+(cd packages/gestalt-datepicker && pnpm build)
+(cd docs && pnpm build-metadata)
 (cd docs && (npx next dev))
 
