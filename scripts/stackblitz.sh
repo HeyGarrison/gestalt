a6523827570ab1e2d300ba671911087a9bf3dd65
HeyGarrison <garrison@hey.com>
2024-02-05 15:44:23 -0600

    run scripts concurrently

--- scripts/stackblitz.sh
+++ scripts/stackblitz.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env sh
 
 # Simplified start script for the stack blitz environment, which can't handle multiple watchers
-set -x
-set -e
+# set -x
+# set -e
 
-(cd packages/gestalt-design-tokens && yarn build)
-(cd packages/gestalt && yarn build)
-(cd packages/gestalt-charts && yarn build)
-(cd packages/gestalt-datepicker && yarn build)
-(cd docs && yarn build-metadata)
-(cd docs && (npx next dev))
+# (cd packages/gestalt-design-tokens && yarn build)
+# (cd packages/gestalt && (yarn install) && (yarn run c))
+# (cd packages/gestalt-charts && yarn build)
+# (cd packages/gestalt-datepicker && yarn build)
+# (cd docs && yarn build-metadata)
+# (cd docs && (yarn install) &&  (npx next dev))
 
+concurrently "(cd packages/gestalt && yarn watch)" "(cd packages/gestalt-charts && yarn watch)" "(cd packages/gestalt-datepicker && yarn watch)" "(cd packages/eslint-plugin-gestalt && yarn watch)" "(cd packages/gestalt-design-tokens && yarn watch)" "(cd docs && yarn dev)"
